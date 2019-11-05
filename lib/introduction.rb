28507e60cdc2b2a87f6de891cac75ba3d88bc730
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-05 20:46:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,19 +1,17 @@
 #lib/introduction.rb
 
-def introduction(name)
-  puts"Hi, my name is#{name}."
-  
+ def introduction(name)
+  puts "Hi, my name is #{name}."
 end
 
-def introduction_to_language(name)(language)
-  puts"Hi, my name is#{name}and I am learning to program in #{language}."
-  
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_to_language_optional(name)(language)
-  puts "The default language will be {Ruby}"
-  
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
 
 
 
